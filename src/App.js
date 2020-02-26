1940aa06bce299d3ceaa93896849da48bd0c606b
kaiylebdev <calebben94@gmail.com>
2020-02-26 01:51:14 +0100

    ecomm-store setups-files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">Column number one
+          <i class="fa fas-home"></i>
+        </div>
+        <div class="col-6">Column number Two</div>
+      </div>
     </div>
   );
 }
